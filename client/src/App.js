255b59081226cd2a68e7eb6bc32bb01e928e4070
joelngyx <joelngyx@gmail.com>
2022-12-27 12:42:50 +0800

    Set up React App

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button onClick={() => {console.log("start")}}>Test Button</button>
+    </div>
+  );
+}
+
+export default App;
