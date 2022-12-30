0acd63b5a2f96309700ca49a9fc8df7dd4d6843a
joelngyx <joelngyx@gmail.com>
2022-12-30 15:49:43 +0800

    Preload assets

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.scss';
 import Landing from './components/Landing';
 import GameLoop from './components/GameLoop';
 import Scoreboard from './components/Scoreboard';
+import BlinkyWGun from "./assets/BlinkyGun.svg";
+import StinkyWGun from "./assets/StinkyGun.svg";
+import Blinky from "./assets/Blinky.svg";
+import Stinky from "./assets/Stinky.svg";
+import BlankBullet from "./assets/BlankBullet.svg";
+import DefaultBullet from "./assets/DefaultBullet.svg";
+import LiveBullet from "./assets/LiveBullet.svg";
+import FiredBullet from "./assets/FiredBullet.svg";
 
 function App() {
-  const [gameState, setGameState] = useState(0);
+  const [gameState, setGameState] = useState(3);
   const [score, setScore] = useState(0);
 
   const startGame = () => {
     setGameState(1);
   }
+
+  useEffect(() => {
+    const imagesToLoad = [
+      BlinkyWGun, StinkyWGun, Blinky, Stinky, BlankBullet,
+      DefaultBullet, LiveBullet, FiredBullet
+    ];
+
+    const cacheImages = async(imgArray) => {
+      const promises = await imgArray.map((src) => {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.src = src;
+          img.onload = resolve();
+          img.onerror = reject();
+        });
+      });
+
+      await Promise.all(promises);
+      setGameState(0);
+    }
+
+    cacheImages(imagesToLoad);
+  }, [])
 
   switch (gameState) {
     case 0:
@@ -20,7 +51,11 @@ function App() {
     case 2:
       return (<Scoreboard gameState={gameState} setGameState={setGameState} score={score} setScore={setScore}/>);
     default:
-      return (<h1>sum tin wong</h1>)
+      return (
+        <div className='loading-page'>
+          Loading...
+        </div>
+      )
   }
 }
 
